f63b17d2212ffe7f6ff607c1a5b4fdfc01c92746
tinto <toso4u@gmail.com>
2021-12-06 11:18:47 +0100

    added the backend server

--- src/Components/TrackForm.js
+++ src/Components/TrackForm.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React , {useContext} from 'react'
 import { Button,Input } from 'react-native-elements'
 import Spacer from './Spacer'
-
+import { Context as LocationContext } from '../Context/LocationContext'
 
 
 
 
 export default function TrackForm() {
+
+
+    const {state: {name,recording,locations},startRecording, stopRecording, changeName} = useContext(LocationContext)
+
+    console.log(locations.length);
     return (
         <>
         <Spacer>
-            <Input  placeholder='Enter Name'/>
+            <Input  placeholder='Enter Name' value={name}  onChangeText={changeName}/>
             </Spacer>
-            <Button title=' start recording'/>
+            {recording ? <Button  title='Stop'  onPress={stopRecording}/> : <Button title=' Start Recording'  onPress={startRecording}/> } 
+            
         </>
     )
 }
